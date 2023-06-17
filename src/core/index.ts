1bb8943d499de617bd2d655f933844ae360a9939
kobayashi <kobayashi@kobayashideMacBook-Pro.local>
2023-06-17 22:20:03 +0800

    chore: use deltaDecorations to replace createDecorationsCollection

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,13 +1,4 @@
-import {
-	Range,
-	Editor,
-	BreakpointEnum,
-	ClearDecorations,
-	IEditorMouseEvent,
-	ModelDeltaDecoration,
-	DecorationsCollection,
-	IMonacoBreakpointPlugin,
-} from '@/types';
+import { Editor, IEditorMouseEvent, IMonacoBreakpointPlugin } from '@/types';
 
 import {
 	MouseTargetType,
@@ -16,9 +7,8 @@ import {
 } from '@/config';
 
 export default class MonacoBreakpointPlugin {
-	/** monaco editor */
+	private hoverDecorationId = '';
 	private editor: Editor | null = null;
-	private hoverDecorationsCollection: DecorationsCollection | null = null;
 	private lineNumberAndDecorationIdMap = new Map<number, string>();
 
 	constructor(params: IMonacoBreakpointPlugin) {
@@ -41,23 +31,26 @@ export default class MonacoBreakpointPlugin {
 		if (!this.editor) return;
 
 		this.editor.onMouseMove((e: IEditorMouseEvent) => {
-			const data = {
-				ranges: this.getHoverBreakpointRanges(),
-				breakpointEnum: BreakpointEnum.Hover,
-			};
+			const model = this.editor?.getModel();
 
-			if (e.target.type === MouseTargetType.GUTTER_GLYPH_MARGIN) {
-				const { range, position } = e.target;
-				const lineNumber = position.lineNumber;
-
-				if (this.getLineDecorationId(lineNumber)) {
-					this.clearDecorations(data);
-					return;
+			if (
+				model &&
+				e.target.type === MouseTargetType.GUTTER_GLYPH_MARGIN
+			) {
+				if (this.hoverDecorationId) {
+					model.deltaDecorations([this.hoverDecorationId], []);
 				}
 
-				this.createHoverBreakpoint(range);
-			} else {
-				this.clearDecorations(data);
+				const newDecoration = {
+					range: e.target.range,
+					options: BREAKPOINT_HOVER_OPTIONS,
+				};
+
+				const decorationIds = model.deltaDecorations(
+					[],
+					[newDecoration]
+				);
+				this.hoverDecorationId = decorationIds[0];
 			}
 		});
 
@@ -94,107 +87,5 @@ export default class MonacoBreakpointPlugin {
 				}
 			}
 		});
-	}
-
-	private createHoverBreakpoint(range: Range) {
-		if (!this.editor) return;
-
-		const decorations: ModelDeltaDecoration[] = [
-			{
-				range,
-				options: BREAKPOINT_HOVER_OPTIONS,
-			},
-		];
-
-		if (this.hoverDecorationsCollection) {
-			this.hoverDecorationsCollection.set(decorations);
-		} else {
-			this.hoverDecorationsCollection =
-				this.editor.createDecorationsCollection(decorations);
-		}
-	}
-
-	private getHoverBreakpointRanges() {
-		return this.hoverDecorationsCollection?.getRanges() ?? [];
-	}
-
-	// private createBreakpoint(range: Range) {
-	// 	if (!this.editor) return;
-
-	// 	const model = this.editor.getModel();
-
-	// 	if (model) {
-	// 		const previousDecorations = this.existDecorationsId.map(
-	// 			(decorationId) => ({
-	// 				range: model.getDecorationRange(decorationId)!,
-	// 				options: model.getDecorationOptions(decorationId)!,
-	// 			})
-	// 		);
-
-	// 		const decorations: ModelDeltaDecoration[] = [
-	// 			{
-	// 				range,
-	// 				options: BREAKPOINT_OPTIONS,
-	// 			},
-	// 			...previousDecorations,
-	// 		];
-
-	// 		console.log(decorations);
-
-	// 		this.existDecorationsId = model.deltaDecorations([], decorations);
-	// 	}
-	// }
-
-	private clearDecorations(params: ClearDecorations) {
-		if (!this.editor) return;
-
-		const decorationIds = [];
-		const { ranges, breakpointEnum } = params;
-		const classname = this.getGlypyMarginClassName(breakpointEnum);
-
-		for (let range of ranges) {
-			const decorations = this.editor.getDecorationsInRange(range) ?? [];
-
-			for (let decoration of decorations) {
-				if (decoration.options.glyphMarginClassName === classname) {
-					decorationIds.push(decoration.id);
-				}
-			}
-		}
-
-		this.editor.removeDecorations(decorationIds);
-	}
-
-	private getLineDecorationId(
-		lineNumber: number,
-		breakpointEnum: BreakpointEnum = BreakpointEnum.Exist
-	) {
-		const decorations = this.editor?.getLineDecorations(lineNumber);
-		const classname = this.getGlypyMarginClassName(breakpointEnum);
-		return decorations?.find(
-			(decoration) =>
-				decoration.options.glyphMarginClassName === classname
-		)?.id;
-	}
-
-	// private getAllDecorations(
-	// 	breakpointEnum: BreakpointEnum = BreakpointEnum.Exist
-	// ) {
-	// 	const classname = this.getGlypyMarginClassName(breakpointEnum);
-	// 	const model = this.editor?.getModel();
-	// 	return (
-	// 		model
-	// 			?.getAllDecorations()
-	// 			.filter(
-	// 				(decoration) =>
-	// 					decoration.options.glyphMarginClassName === classname
-	// 			) ?? []
-	// 	);
-	// }
-
-	private getGlypyMarginClassName(breakpointEnum: BreakpointEnum) {
-		return breakpointEnum === BreakpointEnum.Exist
-			? BREAKPOINT_OPTIONS.glyphMarginClassName
-			: BREAKPOINT_HOVER_OPTIONS.glyphMarginClassName;
 	}
 }
