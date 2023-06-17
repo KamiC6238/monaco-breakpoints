76eb51f7b5fdd02bf673fa3f84065ca932319c89
kobayashi <kobayashi@kobayashideMacBook-Pro.local>
2023-06-17 22:01:09 +0800

    feat: support append breakpoint in glyph margin

--- src/core/index.ts
+++ src/core/index.ts
@@ -18,8 +18,8 @@ import {
 export default class MonacoBreakpointPlugin {
 	/** monaco editor */
 	private editor: Editor | null = null;
-	private existDecorationsCollection: DecorationsCollection | null = null;
 	private hoverDecorationsCollection: DecorationsCollection | null = null;
+	private lineNumberAndDecorationIdMap = new Map<number, string>();
 
 	constructor(params: IMonacoBreakpointPlugin) {
 		if (!params?.editor) {
@@ -41,18 +41,57 @@ export default class MonacoBreakpointPlugin {
 		if (!this.editor) return;
 
 		this.editor.onMouseMove((e: IEditorMouseEvent) => {
+			const data = {
+				ranges: this.getHoverBreakpointRanges(),
+				breakpointEnum: BreakpointEnum.Hover,
+			};
+
 			if (e.target.type === MouseTargetType.GUTTER_GLYPH_MARGIN) {
-				this.createHoverBreakpoint(e.target.range);
+				const { range, position } = e.target;
+				const lineNumber = position.lineNumber;
+
+				if (this.getLineDecorationId(lineNumber)) {
+					this.clearDecorations(data);
+					return;
+				}
+
+				this.createHoverBreakpoint(range);
 			} else {
-				this.clearDecorations({
-					ranges: this.getHoverBreakpointRanges(),
-					breakpointEnum: BreakpointEnum.Hover,
-				});
+				this.clearDecorations(data);
 			}
 		});
 
 		this.editor.onMouseDown((e: IEditorMouseEvent) => {
+			if (!this.editor) return;
+
 			if (e.target.type === MouseTargetType.GUTTER_GLYPH_MARGIN) {
+				const model = this.editor.getModel();
+				const { range, position } = e.target;
+				const lineNumber = position.lineNumber;
+
+				if (!model) return;
+
+				const newDecoration = {
+					range,
+					options: BREAKPOINT_OPTIONS,
+				};
+
+				const decorationId =
+					this.lineNumberAndDecorationIdMap.get(lineNumber);
+
+				if (decorationId) {
+					this.editor.removeDecorations([decorationId]);
+					this.lineNumberAndDecorationIdMap.delete(lineNumber);
+				} else {
+					const decorationIds = model.deltaDecorations(
+						[],
+						[newDecoration]
+					);
+					this.lineNumberAndDecorationIdMap.set(
+						lineNumber,
+						decorationIds[0]
+					);
+				}
 			}
 		});
 	}
@@ -67,11 +106,11 @@ export default class MonacoBreakpointPlugin {
 			},
 		];
 
-		if (!this.hoverDecorationsCollection) {
+		if (this.hoverDecorationsCollection) {
+			this.hoverDecorationsCollection.set(decorations);
+		} else {
 			this.hoverDecorationsCollection =
 				this.editor.createDecorationsCollection(decorations);
-		} else {
-			this.hoverDecorationsCollection.set(decorations);
 		}
 	}
 
@@ -79,15 +118,39 @@ export default class MonacoBreakpointPlugin {
 		return this.hoverDecorationsCollection?.getRanges() ?? [];
 	}
 
+	// private createBreakpoint(range: Range) {
+	// 	if (!this.editor) return;
+
+	// 	const model = this.editor.getModel();
+
+	// 	if (model) {
+	// 		const previousDecorations = this.existDecorationsId.map(
+	// 			(decorationId) => ({
+	// 				range: model.getDecorationRange(decorationId)!,
+	// 				options: model.getDecorationOptions(decorationId)!,
+	// 			})
+	// 		);
+
+	// 		const decorations: ModelDeltaDecoration[] = [
+	// 			{
+	// 				range,
+	// 				options: BREAKPOINT_OPTIONS,
+	// 			},
+	// 			...previousDecorations,
+	// 		];
+
+	// 		console.log(decorations);
+
+	// 		this.existDecorationsId = model.deltaDecorations([], decorations);
+	// 	}
+	// }
+
 	private clearDecorations(params: ClearDecorations) {
 		if (!this.editor) return;
 
 		const decorationIds = [];
 		const { ranges, breakpointEnum } = params;
-		const classname =
-			breakpointEnum === BreakpointEnum.Exist
-				? BREAKPOINT_OPTIONS.glyphMarginClassName
-				: BREAKPOINT_HOVER_OPTIONS.glyphMarginClassName;
+		const classname = this.getGlypyMarginClassName(breakpointEnum);
 
 		for (let range of ranges) {
 			const decorations = this.editor.getDecorationsInRange(range) ?? [];
@@ -100,5 +163,38 @@ export default class MonacoBreakpointPlugin {
 		}
 
 		this.editor.removeDecorations(decorationIds);
+	}
+
+	private getLineDecorationId(
+		lineNumber: number,
+		breakpointEnum: BreakpointEnum = BreakpointEnum.Exist
+	) {
+		const decorations = this.editor?.getLineDecorations(lineNumber);
+		const classname = this.getGlypyMarginClassName(breakpointEnum);
+		return decorations?.find(
+			(decoration) =>
+				decoration.options.glyphMarginClassName === classname
+		)?.id;
+	}
+
+	// private getAllDecorations(
+	// 	breakpointEnum: BreakpointEnum = BreakpointEnum.Exist
+	// ) {
+	// 	const classname = this.getGlypyMarginClassName(breakpointEnum);
+	// 	const model = this.editor?.getModel();
+	// 	return (
+	// 		model
+	// 			?.getAllDecorations()
+	// 			.filter(
+	// 				(decoration) =>
+	// 					decoration.options.glyphMarginClassName === classname
+	// 			) ?? []
+	// 	);
+	// }
+
+	private getGlypyMarginClassName(breakpointEnum: BreakpointEnum) {
+		return breakpointEnum === BreakpointEnum.Exist
+			? BREAKPOINT_OPTIONS.glyphMarginClassName
+			: BREAKPOINT_HOVER_OPTIONS.glyphMarginClassName;
 	}
 }
