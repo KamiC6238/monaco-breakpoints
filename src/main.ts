d175e2a3f8e25c535277b42c7d157b6d83b5e545
kobayashi <823913645@qq.com>
2023-07-03 23:58:16 +0800

    chore: update package.json & vite.config.ts for build library

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,3 @@
-import * as monaco from 'monaco-editor';
-import MonacoBreakpoint from '@/core';
-import '@/style/index.css';
-import '@/style/global.css';
-
-const democode = [
-	'function foo() {\n',
-	'\treturn 1;\n',
-	'}\n',
-	'function bar() {\n',
-	'\treturn 1;\n',
-	'}',
-].join('')
-
-const editor = monaco.editor.create(document.getElementById('app')!, {
-    value: democode,
-    theme: 'vs-dark',
-    automaticLayout: true,
-    minimap: {
-        enabled: false,
-    },
-    glyphMargin: true,
-});
-
-const mncBreakpoint = new MonacoBreakpoint({ editor })
-
-window.mncBreakpoint = mncBreakpoint;
+export * from '@/types';
+export * from '@/config';
+export {default as MonacoBreakpoint} from '@/core';
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,24 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, './src/main.ts'),
+			name: 'index',
+			fileName: 'index'
+		},
+		rollupOptions: {
+			external: ['monaco-editor'],
+			output: {
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals: {
+					'monaco-editor': 'monaco',
+				},
+			},
+		},
+		outDir: path.resolve(__dirname, 'dist')
+	},
 	resolve: {
 		alias: [
 			{
@@ -13,5 +31,5 @@ export default defineConfig({
 				replacement: path.resolve(__dirname, 'src'),
 			},
 		],
-	},
+	}
 });
