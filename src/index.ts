1c134f0a6a14398e6387b7fba4b72fae7457088b
kobayashi <823913645@qq.com>
2023-08-05 18:48:29 +0800

    chore: upload usage img

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
 export {default as MonacoBreakpoint} from './core';
+
+import '@/style/global.css'
+import * as monaco from "monaco-editor";
+import { MonacoBreakpoint } from "monaco-breakpoints";
+
+const democode = [
+  "function foo() {\n",
+  "\treturn 1;\n",
+  "}\n",
+  "function bar() {\n",
+  "\treturn 1;\n",
+  "}",
+].join("");
+
+const editor = monaco.editor.create(document.getElementById("app")!, {
+  value: democode,
+  theme: "vs-dark",
+  glyphMargin: true,
+});
+
+const instance = new MonacoBreakpoint({ editor });
+
+instance.on("breakpointChanged", (breakpoints) => {
+  console.log("breakpointChanged: ", breakpoints);
+});
